37abdf83ffee3c38a2287b2084bf42750c6d4d85
samvel216 <75182258+samvel216@users.noreply.github.com>
2023-02-15 17:46:16 +0300

    12

--- src/index.js
+++ src/index.js
@@ -1,29 +1,9 @@
-document.getElementById("menu-btn").addEventListener("click", function() { 
-    alert("Menu button clicked!"); 
-  });
+const menuBtn = document.querySelector('.menuBtn');
+const menuMobileContainer = document.querySelector('.menuMobileContainer');
 
-  const ruLink = document.getElementById("ru-link"); 
-const uaLink = document.getElementById("ua-link"); 
- console.log(ruLink)
- console.log(uaLink)
-
-const defaultLanguage = localStorage.getItem("language") || "ru"; 
- 
-if (defaultLanguage === "ru") { 
-  ruLink.classList.add("active"); 
-} else { 
-  uaLink.classList.add("active"); 
-} 
- 
-ruLink.addEventListener("click", function (event) { 
-  event.preventDefault(); 
-  localStorage.setItem("language", "ru"); 
-  ruLink.classList.add("active"); 
-  uaLink.classList.remove("active"); 
-}); 
- 
-uaLink.addEventListener("click", function (event) { 
-  event.preventDefault(); 
-  localStorage.setItem("language", "ua"); 
-  uaLink
-})
+menuBtn.addEventListener('click', function () {
+  menuMobileContainer.classList.add('show');
+  setTimeout(function () {
+    menuMobileContainer.classList.remove('show');
+  }, 1000);
+});
