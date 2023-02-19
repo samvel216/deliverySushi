fcb25a95100160dafe48e441533f935c2a3320d1
samvel216 <75182258+samvel216@users.noreply.github.com>
2023-02-19 15:34:04 +0300

    ico

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 const menuBtn = document.querySelector('.menuBtn');
 const menuMobileContainer = document.querySelector('.menuMobileContainer');
-
+const menuCloseBtnEl = document.querySelector('.menuCloseBtn');
 menuBtn.addEventListener('click', function () {
   menuMobileContainer.classList.add('show');
-  setTimeout(function () {
-    menuMobileContainer.classList.remove('show');
-  }, 1000);
+ 
 });
+menuCloseBtnEl.addEventListener('click', function () {
+  menuMobileContainer.classList.remove('show');
+ 
+});
+
+const buttons1 = document.querySelectorAll('.heroButton');
+const heroModalContainer = document.querySelector(".heroModalContainer");
+buttons1.forEach(button => {
+  button.addEventListener('click', () => {
+ 
+    const modalContainer = button.nextElementSibling;
+  
+    heroModalContainer.classList.add('show');
+  });
+});
+
+
+
+
